566a49e2dd5f811af86d0217c388d14df059b2e2
baileyrechkemmer <rechkemmer3@gmail.com>
2023-03-27 16:12:31 -0500

    fixed my mess up on the front end (map function)

--- construction_frontend/src/App.jsx
+++ construction_frontend/src/App.jsx
@@ -11,18 +11,16 @@ const App = () => {
 
 	// create people function
 	const handleCreate = (newConstruction) => {
-		axios
-			.post('http://localhost:8000/api/sites', newConstruction)
-			.then((res) => {
-				console.log(res)
-				getData()
-			})
+		axios.post('http://localhost:8000/sites', newConstruction).then((res) => {
+			console.log(res)
+			getData()
+		})
 	}
 
 	// get construction data
 	const getData = () => {
 		axios
-			.get('http://localhost:8000/api/sites')
+			.get('http://localhost:8000/sites')
 			.then(
 				(res) => setConstruction(res.data),
 				(err) => console.error(err)
@@ -34,7 +32,7 @@ const App = () => {
 
 	const handleDelete = (event) => {
 		axios
-			.delete(`http://localhost:8000/api/sites/${event.target.value}`)
+			.delete(`http://localhost:8000/sites/${event.target.value}`)
 			.then((res) => {
 				getData()
 			})
@@ -44,7 +42,10 @@ const App = () => {
 	const handleUpdate = (editConstruction) => {
 		console.log(editConstruction)
 		axios
-			.put(`http://localhost:8000/api/sites/${editConstruction.id}`, editConstruction)
+			.put(
+				`http://localhost:8000/sites/${editConstruction.id}`,
+				editConstruction
+			)
 			.then((res) => {
 				getData()
 			})
@@ -57,17 +58,20 @@ const App = () => {
 
 	return (
 		<>
-			<h1 className="mainTitle">Job Site Tracker</h1>
-			<h3 className="input">input form</h3>
 			<Add handleCreate={handleCreate} />
-			<div className="container">
-				<h4 className="jobName">job name: {construction.job}</h4>
-				<h4 className="fence">silt fence used: {construction.footage} FT</h4>
-				<Edit className="editBtn" handleUpdate={handleUpdate} construction={construction} />
-				{/* delete button */}
-				<button  className="deleteBtn" onClick={handleDelete} value={construction.id}>
-					delete character
-				</button>
+			<div className='character'>
+				{construction.map((construction) => {
+					return (
+						<div className='' key={construction.id}>
+							<h3>Name: {construction.job}</h3>
+							<h4>feet of silt fence used: {construction.footage}</h4>
+							<Edit handleUpdate={handleUpdate} character={construction} />
+							<button onClick={handleDelete} value={construction.id}>
+								delete site
+							</button>
+						</div>
+					)
+				})}
 			</div>
 		</>
 	)
